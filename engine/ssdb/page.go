0185c177d82a446c4e87a6e87ee8ee6583df1f02
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-02-24 19:55:28 +0900

    define page

--- engine/ssdb/page.go
+++ engine/ssdb/page.go
@@ -1,5 +1,11 @@
 package ssdb
 
+// Page contains multiple tuples in one sequential bytes on the disk.
+type Page struct {
+	Header *PageHeader
+	Tuples []Tuple
+}
+
 type PageHeader struct {
 	TupleCount     uint8
 	TupleLocations []uint16
