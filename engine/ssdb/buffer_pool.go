bb14f030330d8f77137f330348463c1a02820cb8
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-03-10 22:36:36 +0900

    update buffer pool

--- engine/ssdb/buffer_pool.go
+++ engine/ssdb/buffer_pool.go
@@ -2,6 +2,14 @@ package ssdb
 
 import "github.com/dty1er/sdb/lru"
 
-type bufferPool struct {
-	cache *lru.Cache
+type BufferPool struct {
+	frames *lru.Cache
+	// pageTable map[pageID]string
+	// TODO: keep b-tree index here
+}
+
+// PutTuple puts tuple in the buffer pool.
+//
+func (bp *BufferPool) PutTuple(tableName string, tuple *Tuple) {
+
 }
