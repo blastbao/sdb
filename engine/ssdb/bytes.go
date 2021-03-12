81949476aa746d8cbbac98887add8bdca6354630
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-03-12 22:37:13 +0900

    define bytes helper for uint16

--- engine/ssdb/bytes.go
+++ engine/ssdb/bytes.go
@@ -5,6 +5,10 @@ import "encoding/binary"
 // ssdb uses BigEndian as its byteOrder in their byte representation
 var byteOrder = binary.BigEndian
 
+func bytesToUint16(bs []byte) uint16 {
+	return byteOrder.Uint16(bs)
+}
+
 func bytesToUint32(bs []byte) uint32 {
 	return byteOrder.Uint32(bs)
 }
