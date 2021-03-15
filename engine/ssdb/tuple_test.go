0f6bc6682a15165946ce9de2ac29ec0ec2cdc3ea
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-03-15 22:38:44 +0900

    fix tuple test

--- engine/ssdb/tuple_test.go
+++ engine/ssdb/tuple_test.go
@@ -8,19 +8,22 @@ import (
 func Test_Serialize_Deserialize_Tuple(t *testing.T) {
 	tuple := Tuple{Data: []TupleData{
 		{Typ: Int32, Int32Val: 99},
-		{Typ: Char64, Char64Val: [64]byte{'a', 'b', 'c'}},
+		{Typ: Byte64, Byte64Val: [64]byte{'a', 'b', 'c'}},
 	}}
 
-	s, err := SerializeTuple(tuple)
-	if err != nil {
-		t.Errorf("error unexpected: %s", err)
+	s := SerializeTuple(tuple)
+	expected := make([]byte, 4+4+4+64)
+	copy(expected[0:4], []byte{0, 0, 0, 1})   // Type Int32
+	copy(expected[4:8], []byte{0, 0, 0, 99})  // Value 99
+	copy(expected[8:12], []byte{0, 0, 0, 2})  // Type Byte64
+	copy(expected[12:15], []byte{97, 98, 99}) // a, b, c
+	// rest bytes are zero-initialized
+
+	if !reflect.DeepEqual(s, expected) {
+		t.Errorf("unexpected serialized bytes: \n%v, expected: \n%v", s, expected)
 	}
 
-	nt, err := DeserializeTuple(s)
-	if err != nil {
-		t.Errorf("error unexpected: %s", err)
-	}
-
+	nt := DeserializeTuple(s)
 	if len(nt.Data) != 2 {
 		t.Errorf("unexpected length: %d", len(nt.Data))
 	}
@@ -29,7 +32,7 @@ func Test_Serialize_Deserialize_Tuple(t *testing.T) {
 		t.Errorf("unexpected Data[0]: %v", nt.Data[0])
 	}
 
-	if !reflect.DeepEqual(nt.Data[1], TupleData{Typ: Char64, Char64Val: [64]byte{'a', 'b', 'c'}}) {
+	if !reflect.DeepEqual(nt.Data[1], TupleData{Typ: Byte64, Byte64Val: [64]byte{'a', 'b', 'c'}}) {
 		t.Errorf("unexpected Data[1]: %v", nt.Data[1])
 	}
 }
