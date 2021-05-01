db6bc14ba6dc489fab9e9c57ce4d0349096a3ca6
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-05-01 10:53:49 +0900

    more tests for page

--- engine/ssdb/page_test.go
+++ engine/ssdb/page_test.go
@@ -3,7 +3,35 @@ package ssdb
 import (
 	"reflect"
 	"testing"
+
+	"github.com/dty1er/sdb/testutil"
 )
+
+func TestPageHeader_encode(t *testing.T) {
+	ph := &pageHeader{
+		id:          42,
+		tuplesCount: 3,
+		slots: []slot{
+			{offset: 0, length: 10},
+			{offset: 10, length: 25},
+			{offset: 35, length: 50},
+		},
+	}
+
+	encoded := ph.encode()
+
+	testutil.MustEqual(
+		t,
+		encoded,
+		[]byte{
+			0, 0, 0, 42, // page id (4 byte)
+			0, 3, // tuples count (2 byte)
+			0, 0, 0, 10, // slot[0]: offset (2 byte), length (2 byte),
+			0, 10, 0, 25, // slot[1]
+			0, 35, 0, 50, // slot[2]
+		},
+	)
+}
 
 func TestNewPage(t *testing.T) {
 	page := NewPage(42)
@@ -74,5 +102,35 @@ func TestPage_AppendTuple(t *testing.T) {
 		if !reflect.DeepEqual(tp, tuples[i]) {
 			t.Errorf("unexpected deserialized tuple: %v", t)
 		}
+	}
+
+	// make sure err is responded when the page has no space
+	tuple := Tuple{
+		Data: []TupleData{
+			{Typ: Int32, Int32Val: 96},
+			{Typ: Byte64, Byte64Val: [64]byte{'a', 'b', 'c'}},
+		},
+	}
+
+	tupleSize := len(SerializeTuple(tuple))
+	// a page can contains $max tuples
+	// -4 because a page always has 4 byte ID
+	// -2 because a page always has 2 byte tuplesCount
+	// +4 because a slot is 4 byte
+	max := (PageSize - 4 - 2) / (tupleSize + 4)
+
+	page = NewPage(50)
+	// append $max tuples in the page.
+	// Error should not happen.
+	for i := 0; i < max; i++ {
+		if err := page.AppendTuple(tuple); err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+	}
+
+	// because the page already contains $max tuples,
+	// no available space error must happen.
+	if err := page.AppendTuple(tuple); err == nil {
+		t.Errorf("error must happen")
 	}
 }
