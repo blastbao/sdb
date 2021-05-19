1b37a8cbd1c5e2e8a4b46586253470cd92efdb3b
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-05-19 20:11:28 +0900

    fix test script

--- main.go
+++ main.go
@@ -21,24 +21,17 @@ func main() {
 		engine.InsertIndex(idxName, key, t)
 	}
 
-	u1 := &ssdb.Tuple{
-		Data: []*ssdb.TupleData{
-			{Typ: ssdb.Int32, Int32Val: 1},                                        // id
-			{Typ: ssdb.Byte64, Byte64Val: [64]byte{'d', 't', 'y', 'l', 'e', 'r'}}, // name
-			{Typ: ssdb.Int32, Int32Val: 27},                                       // age
-		},
-	}
+	for i := 1; i <= 5; i++ {
+		u := &ssdb.Tuple{
+			Data: []*ssdb.TupleData{
+				{Typ: ssdb.Int32, Int32Val: int32(i)},                                 // id
+				{Typ: ssdb.Byte64, Byte64Val: [64]byte{'d', 't', 'y', 'l', 'e', 'r'}}, // name
+				{Typ: ssdb.Int32, Int32Val: 27},                                       // age
+			},
+		}
 
-	u2 := &ssdb.Tuple{
-		Data: []*ssdb.TupleData{
-			{Typ: ssdb.Int32, Int32Val: 2},                                             // id
-			{Typ: ssdb.Byte64, Byte64Val: [64]byte{'d', 't', 'y', 'l', 'e', 'r', '2'}}, // name
-			{Typ: ssdb.Int32, Int32Val: 28},                                            // age
-		},
+		insert("users", "users_id", i, u)
 	}
-
-	insert("users", "users_id", 1, u1)
-	insert("users", "users_id", 2, u2)
 
 	if err := engine.Shutdown(); err != nil {
 		fmt.Fprintf(os.Stderr, "shutdown engine: %s\n", err)
