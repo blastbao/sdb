08b7d75ed91cf0dcc407cf88a8c2a1c5968669b5
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-05-26 12:24:38 +0900

    fix test script

--- main.go
+++ main.go
@@ -4,37 +4,25 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/dty1er/sdb/engine"
+	"github.com/dty1er/sdb/cmd/cli"
 )
 
+var queries = []string{
+	"Create table users (id int64 primary key, name string);",
+}
+
 func main() {
-	e, err := engine.New()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "init engine: %s\n", err)
-		return
-	}
-
-	e.CreateIndex("users_id")
-
-	insert := func(table, idxName string, t *engine.Tuple) {
-		e.InsertTuple(table, t)
-		e.InsertIndex(idxName, t)
-	}
-
-	for i := 1; i <= 5; i++ {
-		u := &engine.Tuple{
-			Data: []*engine.TupleData{
-				{Typ: engine.Int32, Int32Val: int32(i), Key: true},                      // id
-				{Typ: engine.Byte64, Byte64Val: [64]byte{'d', 't', 'y', 'l', 'e', 'r'}}, // name
-				{Typ: engine.Int32, Int32Val: 27},                                       // age
-			},
+	for _, query := range queries {
+		resp, err := cli.ExecQuery(query)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+			continue
+		}
+		if resp.Result != "OK" {
+			fmt.Fprintf(os.Stdout, "execution failed: %s\n", resp.Error.Message)
+			continue
 		}
 
-		insert("users", "users_id", u)
-	}
-
-	if err := e.Shutdown(); err != nil {
-		fmt.Fprintf(os.Stderr, "shutdown engine: %s\n", err)
-		return
+		fmt.Fprintf(os.Stdout, "%+v\n", resp.RS)
 	}
 }
